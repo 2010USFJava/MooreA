c399ae8bc12af0027f948ae4c764491832247d95
amsaizan <angie.m.moore@outlook.com>
2020-10-27 13:02:31 -0500

    updating maven

--- Mayhem/src/com/revature/constructor/ConstructorDemo.java
+++ Mayhem/src/com/revature/constructor/ConstructorDemo.java
@@ -4,22 +4,26 @@ public class ConstructorDemo {
 
 	public static void main(String[] args) {
 	
-		//C c= new C();
-
+		//C c = new C();
+	}
 	
-
-
-
  class A{
+	A(){
+	System.out.println("in A");	
+	}
 
 }
 
  class B extends A{
+	 B(){
+		 System.out.println("in B");
+	 }
 	
 }
 
  class C extends B{
-	
-}
+	C(){
+	System.out.println("in C");	
+	}
 }
 }
